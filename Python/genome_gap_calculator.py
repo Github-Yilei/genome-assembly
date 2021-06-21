7e1d13e1c35f7f199d84e0a369350c98b59f908f
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-06-21 17:17:31 +0800

    Update genome_gap_calculator.py

--- Python/genome_gap_calculator.py
+++ Python/genome_gap_calculator.py
@@ -1,3 +1,6 @@
+#/usr/bin/python
+#_*_coding: utf-8 _*_
+
 """
 @ Description: the gaps of a genomes was filled with "N". 
 @ Usage: python genome_gap_calculator.py input.file >output.file
@@ -5,26 +8,45 @@
 @ Date: 2020-10-14
 """
 
-
 import re
 import sys
-import_file = sys.argv[1]
+import argparse
 
-summary = 0
-lines = ""
-i = 0
-
-with open (import_file, "r") as genome_fa:
-    for line in genome_fa:
-        if line.startswith(">"):
-            line = line.split()[0]
-        else:
-            line = line.replace("\n", "")
-        lines += line
-
-    for match in re.finditer("N+", lines):
-        i += 1
-        gap_length = match.end() - match.start()
-        summary = summary + gap_length
+def genome_gap_calculator(args):
+    summary = 0
+    i = 0
+		results = "index\tchr_id\tstart\tend gap_length\tsummary\n"
+    with open (args.input_path, "r") as genome_fa:
+        for line in genome_fa:
+						whole_line = ''
+            if line.startswith(">"):
+                chr_id = line.split()[0].replace(">", "")
+            else:
+                whole_line = line.replace("\n", "")
+  
+            for match in re.finditer("N+", whole_line):
+                i += 1
+                gap_length = match.end() - match.start()
+                summary += gap_length
+                results += str(i) + "\t" + chr_id + "\t" + str(match.start()) + "\t" + str(match.end()) + "\t" + str(gap_length) + "\t" + str(summary) + "\n"
+            
+    with open(args.output, 'w') as out_file:
+        out_file.write(results )
         
-        print(i, match.start(), match.end(), gap_length, summary)
+  
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description =  "counting the length of gap and building a ploting-ready data frame for R")
+    parser.add_argument('--input_path',
+	                    dest = 'input_path',
+						help = 'The absolute path of the genome fasta')
+	
+		parser.add_argument('--output', 
+                        dest = "output", 
+                        help = 'The result file in tsv format')
+    
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        sys.exit()
+    args = parser.parse_args()
+    
+    genome_gap_calculator(args)
