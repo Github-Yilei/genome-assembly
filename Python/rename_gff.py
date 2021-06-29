6382504c86f5f80998257a9886b027f7557df223
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-06-29 13:59:48 +0800

    Update rename_gff.py

--- Python/rename_gff.py
+++ Python/rename_gff.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 _*_
 
 """
-@ Description: rename the IDs of the gff3 file from EVidenceModeler.
+@ Description: rename the IDs of the gff3 file from EVidenceModeler. if you want a 
 @ Usage: python rename_gff.py --input_file --prefix --output
 @ Author: Yilei
 @ Date:2020-01-13
@@ -22,26 +22,26 @@ def rename_gff(args):
 		for line in gff:
 			if not line.startswith("\n"):
 				records = line.split("\t")
-				records[1] = "."
+				records[1] = "EVM"
 			if re.search(r"\tgene\t", line):
 				count = count + 10
 				mRNA  = 0
 				gene_id = prefix + str(count).zfill(6)
-				records[8] = "ID={}".format(gene_id)
+				records[8] = "ID={};Name={}".format(gene_id, gene_id)
 			elif re.search(r"\tmRNA\t", line):
 				cds   = 0
 				exon  = 0
 				mRNA  = mRNA + 1
 				mRNA_id  = gene_id + "." + str(mRNA)
-				records[8] = "ID={};Parent={}".format(mRNA_id, gene_id)
+				records[8] = "ID={};Parent={};Name={}".format(mRNA_id, gene_id, mRNA_id)
 			elif re.search(r"\texon\t", line):
 				exon = exon + 1
 				exon_id  = mRNA_id + "_exon_" + str(exon)
-				records[8] = "ID={};Parent={}".format(exon_id, mRNA_id)
+				records[8] = "ID={};Parent={};Name={}".format(exon_id, mRNA_id, exon_id)
 			elif re.search(r"\tCDS\t", line):
 				cds = cds + 1
 				cds_id  = mRNA_id + "_cds_" + str(cds)
-				records[8] = "ID={};Parent={}".format(cds_id, mRNA_id)
+				records[8] = "ID={};Parent={};Name={}".format(cds_id, mRNA_id, cds_id)
 			else:
 				continue
 			with open(args.output, "a") as new_gff:
