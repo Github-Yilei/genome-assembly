0041bf3f690cbb396e6261ae77d143ae2ada3a65
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-07-15 11:43:20 +0800

    Update bwa_mem_allhic.sh

--- Shell/bwa_mem_allhic.sh
+++ Shell/bwa_mem_allhic.sh
@@ -2,24 +2,26 @@
 ~/miniconda3/bin/samtools faidx draft.asm.fasta 
 
 # building atg_tabel
-~/miniconda3/envs/allhic_env/bin/gmap_build -D . -d DB draft.asm.fasta 
-~/miniconda3/envs/allhic_env/bin/gmap -D . -d DB -t 12 -f 2 -n 2 reference.cds.fasta > gmap.gff3
-perl gmap2AlleleTable.pl ref_gene.gff3
+#~/miniconda3/envs/allhic_env/bin/gmap_build -D . -d DB draft.asm.fasta 
+#~/miniconda3/envs/allhic_env/bin/gmap -D . -d DB -t 12 -f 2 -n 2 reference.cds.fasta > gmap.gff3
+#perl gmap2AlleleTable.pl ref_gene.gff3
 
 # filtering sam
-~/biosoft/ALLHiC/scripts/PreprocessSAMs.pl bwa_mem.sam draft.asm.fasta MBOI
-~/miniconda3/bin/samtools view -t -b bwa_mem.REduced.paired_only.bam > sampe.clean.sam
-~/miniconda3/bin/samtools view -b -t draft.asm.fasta.fai sampe.clean.sam > sampe.clean.bam
+~/biosoft/ALLHiC/scripts/PreprocessSAMs.pl bwa_mem.sam draft.asm.fasta HINDIII
+~/miniconda3/bin/samtools view -t -b -@ 10 bwa_mem.REduced.paired_only.bam > sampe.clean.sam
+~/miniconda3/bin/samtools view -b -t -@ 10 draft.asm.fasta.fai sampe.clean.sam > sampe.clean.bam
 
 # Partition
 ~/biosoft/ALLHiC/bin/ALLHiC_partition -b sampe.clean.bam -r draft.asm.fasta -e AAGCTT -k 9 
 
 # Optimize 
-~/biosoft/ALLHiC/bin/allhic extract sampe.clean.bam draft.asm.fasta --RE AAGCTT 
+~/biosoft/ALLHiC/bin/allhic extract sampe.clean.bam draft.asm.fasta --RE AAGCTT
 
+
+### optimize
 rm cmd.list
-for i in group*.txt; 
-do echo "~/biosoft/ALLHiC/bin/allhic optimize $i sampe.clean.clm" >> cmd.list
+for((K=1;K<=9;K++))
+do echo "~/biosoft/ALLHiC/bin/allhic optimize sampe.clean.counts_AAGCTT.9g${K}.txt sampe.clean.clm" >> cmd.list
 done
 ~/miniconda3/pkgs/parafly-r2013_01_21-1/bin/ParaFly -c cmd.list -CPU  9
 
@@ -28,7 +30,6 @@ done
 
 # plot
 ~/miniconda3/bin/samtools faidx groups.asm.fasta
-cut -f1,2 groups.asm.fasta.fai|grep sample > chrn.list
+cut -f1,2 groups.asm.fasta.fai| grep AAGCTT > chrn.list
 
-~/biosoft/ALLHiC/bin/ALLHiC_plot sampe.bwa_mem.REduced.paired_only.bam groups.agp chrn.list 500k pdf
 ~/biosoft/ALLHiC/bin/ALLHiC_plot sampe.clean.bam groups.agp chrn.list 500k pdf
