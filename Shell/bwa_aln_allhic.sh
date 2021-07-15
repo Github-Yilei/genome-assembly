6c71594a293282cd2fb4e53227b5fe691d5e9915
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-07-15 19:57:01 +0800

    Update bwa_aln_allhic.sh

--- Shell/bwa_aln_allhic.sh
+++ Shell/bwa_aln_allhic.sh
@@ -1,32 +1,19 @@
 ### combine SAM
-
+grep ^@ sampe_part1_aln.sam >head
+ls | grep aln.sam | while read id; do grep -v ^@ $id ; done > combined.sam
+cat head combined.sam > combined_aln.sam
+rm head combined.sam
 
 ### Filtering SAM
-~/biosoft/ALLHiC/scripts/PreprocessSAMs.pl sampe_part${P}_aln.sam draft.asm.fasta HINDIII
-
-
-rm cmd.list
-for((P=1;P<=8;P++))
-do
-echo "~/biosoft/ALLHiC/scripts/filterBAM_forHiC.pl sampe_part${P}_aln.REduced.paired_only.bam sampe_part${P}.clean.sam" >> cmd.list
-done
-~/miniconda3/pkgs/parafly-r2013_01_21-1/bin/ParaFly -c cmd.list -CPU  8
-
-rm cmd.list
-for((P=1;P<=8;P++))
-do
-echo "~/miniconda3/bin/samtools view -bt draft.asm.fasta.fai sampe_part${P}.clean.sam >sampe_part${P}.clean.bam" >> cmd.list
-done
-~/miniconda3/pkgs/parafly-r2013_01_21-1/bin/ParaFly -c cmd.list -CPU  8
-
-~/miniconda3/bin/samtools merge sampe_part1.clean.bam sampe_part2.clean.bam sampe_part3.clean.bam sampe_part4.clean.bam sampe_part5.clean.bam \
-	sampe_part6.clean.bam sampe_part7.clean.bam sampe_part8.clean.bam -O BAM -@ 10 >sampe.clean.bam
+~/biosoft/ALLHiC/scripts/PreprocessSAMs.pl combined_aln.sam draft.asm.fasta HINDIII
+~/biosoft/ALLHiC/scripts/filterBAM_forHiC.pl combined_aln.REduced.paired_only.bam sample.clean.sam
+~/miniconda3/bin/samtools view -bt draft.asm.fasta.fai sample.clean.sam > sample.clean.bam
 
 ### Prune	
 ~/biosoft/ALLHiC/bin/ALLHiC_prune -i Allele.ctg.table -b sampe.clean.bam -r draft.asm.fasta
 
 ### Partition
-~/biosoft/ALLHiC/bin/ALLHiC_partition -b prunning.bam -r draft.asm.fasta -e AAGCTT -k 9
+~/biosoft/ALLHiC/bin/ALLHiC_partition -b prunning.bam -r draft.asm.fasta -e AAGCTT -k 18
 
 ### Rescue
 ~/biosoft/ALLHiC/bin/ALLHiC_rescue -b sampe.clean.bam -r draft.asm.fasta -c clusters.txt -i counts_RE.txt 
@@ -35,10 +22,10 @@ done
 ~/biosoft/ALLHiC/bin/allhic extract sampe.clean.bam draft.asm.fasta --RE AAGCTT
 
 rm cmd.list
-for((K=1;K<=9;K++))
-do echo "~/biosoft/ALLHiC/bin/allhic optimize sampe.clean.counts_AAGCTT.9g${K}.txt sampe.clean.clm" >> cmd.list
+for((K=1;K<=18;K++))
+do echo "~/biosoft/ALLHiC/bin/allhic optimize sampe.clean.counts_AAGCTT.18g${K}.txt sampe.clean.clm" >> cmd.list
 done
-~/miniconda3/pkgs/parafly-r2013_01_21-1/bin/ParaFly -c cmd.list -CPU  9
+~/miniconda3/pkgs/parafly-r2013_01_21-1/bin/ParaFly -c cmd.list -CPU  18
 
 # Build
 ~/biosoft/ALLHiC/bin/ALLHiC_build draft.asm.fasta
