8b4fae9f0e38f5fddc3eff1888dbcae477c86bd9
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-07-24 10:53:15 +0800

    Update mumer_dot.R

--- R/mumer_dot.R
+++ R/mumer_dot.R
@@ -2,28 +2,21 @@ file <- "C:\\Users\\Admin\\Desktop\\"
 df1 <- read.delim(paste0(file, "tri_chr_filtered.coords"), header = F, 
                        sep = "\t",
                        stringsAsFactors = FALSE)
+
 colnames(df1) <- c("ref_start", "ref_end", "qry_start", "qry_end", "ref_len", "qry_len", 
                   "identiy", "ref_tag","qry_tag" )
 
 df <- subset(df1,ref_tag == "sampe.unique.REduced.paired_only.counts_AAGCTT.9g1" & 
-               qry_tag == "sampe.unique.REduced.paired_only.counts_AAGCTT.9g1_RagTag_2")
-x_range <- range(c(df$ref_start, df$ref_end))
-y_range <- range(c(df$qry_start, df$qry_end))
-plot.new()
+               qry_tag == "sampe.unique.REduced.paired_only.counts_AAGCTT.9g1_RagTag_1")
 
-plot.window(xlim = x_range,
-            ylim = y_range)
 
-for( i in 1:nrow(df)){
-  
-  if (df[i,3] < df[i,4]){
-    lines(x = df[i,1:2], y = df[i,3:4], col = "red")
-  } else{
-    lines(x = df[i,1:2], y = df[i,3:4], col = "blue")
-  }
-  
-}
+df$structure <- ifelse(df[,3] < df[,4], "normal", "inversion")
 
-box()
-axis(1, at = seq(0, x_range[2], 10000), labels = seq(0, x_range[2], 10000) / 10000)
-axis(2, at = seq(0, y_range[2], 10000), labels = seq(0, y_range[2], 10000) / 10000)
+
+ggplot(data = df) + geom_segment(aes(x = ref_start/10000, y = qry_start/10000,
+                                     xend = ref_end/10000, yend = qry_end/10000, 
+                                     col = structure), size = 1) + 
+  scale_color_manual(values = c("blue", "red")) + 
+  theme_bw() + 
+  xlab(label = "Canu chr1") +
+  ylab(label = "Hap1 chr1")
