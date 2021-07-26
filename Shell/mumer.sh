3dbc35822f2d12d3482b2a0e6c949116e9e1923a
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-07-26 15:51:18 +0800

    Create mumer.sh

---
+++ Shell/mumer.sh
@@ -0,0 +1,14 @@
+Mummer=/share/home/stu_wuyilei/biosoft/ppsPCP_file/mummer-4.0.0beta2
+RequiredCPU=5
+
+
+${Mummer}/nucmer -p ${sampleID} -t ${RequiredCPU} reference.fa query.fa
+${Mummer}/delta-filter -1 -q -r  ref_qry.delta > ref_qry_filtered.delta
+${Mummer}/show-coords ref_qry_filtered.delta > ref_qry_filtered.coords
+
+# dot plot
+mumer_dot.R
+
+# snp vcf
+${Mummer}/show-snps -C -T -l -r ref_qry_filtered.delta > ref_qry_filtered.snps
+python3 mummer2vcf.py --input-header -s ref_qry_filtered.snp -g reference.fa --output-header > qry_snp.vcf
