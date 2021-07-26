ec34725076ca742e7d058e811e73f1a2654a18b2
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-07-26 13:33:39 +0800

    Update mumer_dot.R

--- R/mumer_dot.R
+++ R/mumer_dot.R
@@ -12,11 +12,32 @@ df <- subset(df1,ref_tag == "sampe.unique.REduced.paired_only.counts_AAGCTT.9g1"
 
 df$structure <- ifelse(df[,3] < df[,4], "normal", "inversion")
 
-
+# show one chromosome
 ggplot(data = df) + geom_segment(aes(x = ref_start/10000, y = qry_start/10000,
                                      xend = ref_end/10000, yend = qry_end/10000, 
                                      col = structure), size = 1) + 
   scale_color_manual(values = c("blue", "red")) + 
   theme_bw() + 
-  xlab(label = "Canu chr1") +
-  ylab(label = "Hap1 chr1")
+  xlab(label = "ref chr1") +
+  ylab(label = "qry chr1")
+
+# show all chromosomes
+ggplot(data = df) + facet_grid(qry_tag~ref_tag, as.table=FALSE, 
+                                switch = "both", scales="free", 
+                                space="free") + 
+  geom_segment(aes(x = ref_start/10000, y = qry_start/10000,
+                                     xend = ref_end/10000, 
+                   yend = qry_end/10000, color = structure), size = 1) +
+
+  scale_color_manual(values = c("blue", "red")) + 
+  theme_bw() + 
+  xlab(label = "ref genome") +
+  ylab(label = "qry genome") +
+  theme_bw() + 
+  theme(panel.spacing = unit(0,"lines"),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          panel.grid = element_blank(),
+          panel.background =element_blank(),
+    )
