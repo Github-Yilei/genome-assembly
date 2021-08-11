08de4b825a09b53c04a8d5cfdcde661d68af76f0
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-08-11 13:44:48 +0800

    Update mumer.sh

--- Shell/mumer.sh
+++ Shell/mumer.sh
@@ -1,16 +1,15 @@
-Mummer=/share/home/stu_wuyilei/biosoft/ppsPCP_file/mummer-4.0.0beta2
-RequiredCPU=5
 
+Mummer=~/miniconda3/bin/nucmer
 
 ${Mummer}/nucmer -p ${sampleID} -t ${RequiredCPU} reference.fa query.fa
 ${Mummer}/delta-filter -1 -q -r  ref_qry.delta > ref_qry_filtered.delta
 ${Mummer}/show-coords -q -r -T -d -H -c ref_qry_filtered.delta > ref_qry_filtered.coords
 
 # dot plot
-mumer_dot.R
+#mumer_dot.R
 
 # snp vcf
 ${Mummer}/show-snps -C -T -l -r ref_qry_filtered.delta > ref_qry_filtered.snps
 python3 mummer2vcf.py --input-header -s ref_qry_filtered.snp -g reference.fa --output-header > qry_snp.vcf
 
-python3 SNP_calculater.py 
+# python3 SNP_calculater.py 
