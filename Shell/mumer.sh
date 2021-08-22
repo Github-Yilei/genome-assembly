6d049ce316c2a066e103b4c21ee7774f40065b32
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-08-22 21:38:15 +0800

    Update mumer.sh

--- Shell/mumer.sh
+++ Shell/mumer.sh
@@ -13,3 +13,7 @@ ${Mummer}/show-snps -C -T -l -r ref_qry_filtered.delta > ref_qry_filtered.snps
 python3 mummer2vcf.py --input-header -s ref_qry_filtered.snp -g reference.fa --output-header > qry_snp.vcf
 
 # python3 SNP_calculater.py 
+
+for((K=1;K<=9;K++));
+do echo "/share/home/stu_wuyilei/miniconda3/bin/nucmer --mum -l 1000 -c 200 -g 200  -p Chr${K} Hap1Chr/chr${K}_RagTag.fasta Hap2Chr/chr${K}_RagTag.fasta && show-snps -Clr Chr${K}.delta > Chr${K}.snps" >> cmd.list; 
+done
