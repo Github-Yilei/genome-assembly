0af0565594b918329c6378a037c7bf67d8781f8d
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2021-08-23 16:07:18 +0800

    Update dotdup_remover.py

--- Python/dotdup_remover.py
+++ Python/dotdup_remover.py
@@ -12,37 +12,37 @@ import argparse
 
 def dot_remover(args):
 	gene_dict = {}
-    with open(args.input, 'r') as lines:
-        for line in lines:
-            if line.startswith('>'):
-                temp_line = ""
-                gene_id = line.strip('>\n').split('.')[0]
-                flag = gene_id
-                try:
-                    gene_dict[gene_id]
-                except KeyError:
-                    gene_dict[gene_id] = [line]
-                else:
-                    gene_dict[gene_id].append(line)
-            else:
-                line = line.strip("\n")
-                gene_dict[flag][-1] += line
-        # select the longest sequence
-    longest_seq = ""
-    for k,v in gene_dict.items():
-        if len(v) == 1:
-            longest_seq += gene_dict[k][0].strip('.') + "\n"
-        else:
-            trans_max = ''
-            for trans in gene_dict[k]:
-                a = len(list(trans))
-                b = len(list(trans_max))
-                if a > b:
-                    trans_max = trans
-                longest_seq += trans_max
+	with open(args.input, 'r') as lines:
+		for line in lines:
+			if line.startswith('>'):
+				temp_line = ""
+				gene_id = line.strip('>\n').split('.')[0]
+				flag = gene_id
+				try:
+					gene_dict[gene_id]
+				except KeyError:
+					gene_dict[gene_id] = [line]
+				else:
+					gene_dict[gene_id].append(line)
+			else:
+				line = line.strip("\n")
+				gene_dict[flag][-1] += line
+    # select the longest sequence
+	longest_seq = ""
+	for k,v in gene_dict.items():
+		if len(v) == 1:
+			longest_seq += gene_dict[k][0].strip('.') + "\n"
+		else:
+			trans_max = ''
+			for trans in gene_dict[k]:
+				a = len(list(trans))
+				b = len(list(trans_max))
+				if a > b:
+					trans_max = trans
+				longest_seq += trans_max
 
-    with open(args.output, 'w') as longest:
-        longest.write(longest_seq)
+	with open(args.output, 'w') as longest:
+		longest.write(longest_seq)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'This a protein sequence cleaner.')
