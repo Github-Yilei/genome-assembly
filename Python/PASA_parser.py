ac1a6aceb65138088e03da6b88ebd73daf952865
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2022-05-11 15:51:06 +0800

    Update the script, faster!!!!!!

--- Python/PASA_parser.py
+++ Python/PASA_parser.py
@@ -4,64 +4,48 @@ input_file = sys.argv[1]
 pep_out = sys.argv[2]
 gff_out = sys.argv[3]
 
-gff_list =  list()
+gff_dict =  dict()
 uniq_id_list = list()
+
 with open(input_file, 'r') as gff3:
-    temp_pep = dict()
     for line in gff3:
         if line == '\n':
             continue
         elif line.startswith('# ORIGINAL') or line.startswith('# PASA_UPDATE'):
-            temp_pep = dict()
+            gff_key = ''
+            temp_pep = ''
+            temp = {}
         elif line.startswith('chr'):
-            temp = dict()
-            ID = ''
             line_spl = line.split("\t")
             line_spl[1] = 'PASA'
-            chrom = line_spl[0]
-            start = line_spl[3]
-            end = line_spl[4]
+            gff_value = '\t'.join(line_spl[:-1]) + "\tPlaceholders"
 
             if line_spl[2] == 'gene':
-                ID = 'a'
-                temp_pep['chrom'] = chrom
-                temp_pep['start'] = start
-                temp_pep['end'] = end
-                temp_pep['ID'] = 'g'
-                temp_pep['GFF'] = ''
-            elif line_spl[2] == 'mRNA':
-                ID = 'b'
-            elif line_spl[2] == 'five_prime_UTR':
-                ID = 'c'
-            elif line_spl[2] == 'exon':
-                ID = 'd'
-            elif line_spl[2] == 'CDS':
-                ID = 'e'
-            elif line_spl[2] == 'three_prime_UTR':
-                ID = 'f'
+                chrom = line_spl[0]
+                start = line_spl[3]
+                end = line_spl[4]
 
-            temp['chrom'] = chrom
-            temp['start'] = start
-            temp['end'] = end
-            temp['ID'] = ID
-            temp['GFF'] = '\t'.join(line_spl[:-1]) + "\tPlaceholders"
-            # uniq lines
-            if temp['GFF'] not in uniq_id_list:
-                gff_list.append(temp)
-                uniq_id_list.append(temp['GFF'])
+                gff_key = chrom + '_' + start +'_' + end
+                temp = {'chrom' : chrom, 'start' : start, 'end' : end}
+
+                uniq_id_list.append(temp)
+                gff_dict[gff_key] = [gff_value]
+            else:
+                gff_dict[gff_key].append(gff_value)
+
         elif line.startswith('#PROT'):
             pep_spl = line.split("\t")
             pep_seq = pep_spl[1]
             # if this is the first prot
-            if len(temp_pep['GFF']) == 0:
-                temp_pep['GFF'] = 'represnt_pep' + "\t" + pep_seq
-                gff_list.append(temp_pep)
+            if len(temp_pep) == 0:
+                temp_pep = 'represnt_pep' + "\t" + pep_seq
+                gff_dict[gff_key].append(temp_pep)
 
-            elif len(pep_seq) > len(temp_pep['GFF']):
-                temp_pep['GFF'] = 'represnt_pep' + "\t" + pep_seq
-                gff_list[-1] = temp_pep
-
-sorted_list = sorted(gff_list, key=lambda k: (k['chrom'], int(k['start']), k['ID']))
+            elif len(pep_seq) > len(temp_pep):
+                temp_pep = 'represnt_pep' + "\t" + pep_seq
+                gff_dict[gff_key][-1] = temp_pep
+# sort keys
+sorted_list = sorted(uniq_id_list, key=lambda k: (k['chrom'], int(k['start'])))
 
 count = 0
 mRNA  = 0
@@ -72,8 +56,11 @@ UTR_3 = 0
 prefix = "Cp"
 
 for i in range(len(sorted_list)):
-        temp = sorted_list[i]
-        line = temp['GFF'].strip()
+    temp = sorted_list[i]
+    gff_key = temp['chrom'] + '_' + temp['start'] + '_' + temp['end']
+    temp_list = gff_dict[gff_key]
+    for i in range(len(temp_list)):
+        line = temp_list[i]
         records = line.split("\t")
         if re.search(r"\tgene\t", line):
             count = count + 10
@@ -89,7 +76,7 @@ for i in range(len(sorted_list)):
         elif line.startswith('represnt_pep'):
             pep_spl = line.split("\t")
             pep_seq = pep_spl[1].strip("*")
-            pep_records = pep_id + pep_seq  + "\n"
+            pep_records = pep_id + pep_seq
             with open(pep_out, "a") as pep_file:
                 pep_file.write(pep_records)
             pep_id = ''
@@ -122,4 +109,3 @@ for i in range(len(sorted_list)):
         if len(records ) == 9:
             with open(gff_out, "a") as new_gff:
                 new_gff.write("\t".join(records) +'\n')
-
