8d0d81bc51230433671c4aa893a99b16aa847e6c
Github-Yilei <82864917+Github-Yilei@users.noreply.github.com>
2023-11-08 17:23:01 +0800

    Update EDTAsum2tsv.py

--- Python/EDTAsum2tsv.py
+++ Python/EDTAsum2tsv.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 # file_path：the ancestor path of EDTA result
+
 file_path = sys.argv[1]
 
 sample_idx = {}
@@ -19,31 +20,35 @@ for sample in sample_idx.keys():
             line_spl = line.strip().split()
             i += 1
             # LTR
-            if i == 8:
+            if "Copia" in line_spl:
                 print(sample + "\t" + "ClassI\tLTR\tCopia\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 9:
+            if "Gypsy" in line_spl:
                 print(sample + "\t" + "ClassI\tLTR\tGypsy\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 10:
+            if "unknown" in line_spl and i < 15:
                 print(sample + "\t" + "ClassI\tLTR\tunknown\t" + "\t".join(str(e) for e in  line_spl[-3:]))
             # TIR
-            if i == 12:
+            if "CACTA" in line_spl:
                 print(sample + "\t" + "ClassII\tTIR\tCACTA\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 13:
+            if "Mutator" in line_spl:
                 print(sample + "\t" + "ClassII\tTIR\tMutator\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 14:
+            if "PIF_Harbinger" in line_spl:
                 print(sample + "\t" + "ClassII\tTIR\tPIF_Harbinger\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 15:
+            if "Tc1_Mariner" in line_spl:
                 print(sample + "\t" + "ClassII\tTIR\tTc1_Mariner\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 16:
+            if "hAT" in line_spl:
                 print(sample + "\t" + "ClassII\tTIR\thAT\t" + "\t".join(str(e) for e in  line_spl[-3:]))
+            #if "polinton" in line_spl:    
+            #    print(sample + "\t" + "ClassII\tTIR\tpolinton\t" + "\t".join(str(e) for e in  line_spl[-3:]))
             # nonLTR
-            if i == 18:
+            if "LINE_element" in line_spl:
                 print(sample + "\t" + "ClassI\tnonLTR\tLINE_element\t" + "\t".join(str(e) for e in  line_spl[-3:]))
-            if i == 19:
+            if "unknown" in line_spl and i > 15:
                 print(sample + "\t" + "ClassI\tnonLTR\tunknown\t" + "\t".join(str(e) for e in  line_spl[-3:]))
             # nonTIR
-            if i == 21:
+            if "thelitron" in line_spl:
                 print(sample + "\t" + "ClassII\tnonTIR\thelitron\t" + "\t".join(str(e) for e in  line_spl[-3:]))
             # repeat_region
-            if i == 22:
+            if "repeat_region" in line_spl:
                 print(sample + "\t" + "Repeat\trepeat_region\trepeat_region\t" + "\t".join(str(e) for e in  line_spl[-3:]))
+
+
